eb2f02901c3b0aa05ab29153497e1878e807deb0
Anuj <invincibleanuj1718@gmail.com>
2024-09-20 22:24:43 +0530

    MainNav/fixed some bugs

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -3,7 +3,6 @@ import { Button } from "./ui/button";
 import UsernameMenu from "./UsernameMenu";
 
 export default function MainNav() {
-<<<<<<< HEAD
   const { loginWithRedirect, isAuthenticated } = useAuth0();
   return (
     <span className="flex space-x-2 items-center">
@@ -19,16 +18,5 @@ export default function MainNav() {
         </Button>
       )}
     </span>
-=======
-  const { loginWithRedirect } = useAuth0();
-  return (
-    <Button
-      variant="ghost"
-      className="font-bold hover:text-orange-500 hover:bg-white"
-      onClick={async () => await loginWithRedirect()}
-    >
-      Log In
-    </Button>
->>>>>>> 35b5a0a930b2751bcf2b2c1f59f48a9da9b1e960
   );
 }
