b1a26d9a0ea5d18154739bf0b39a1797d03bbb34
Anuj <invincibleanuj1718@gmail.com>
2024-09-22 13:04:55 +0530

    Auth/Modified

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,9 +1,11 @@
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
   children: React.ReactNode;
 };
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
+  const navigate = useNavigate();
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
@@ -11,6 +13,7 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
     throw new Error("unable to initialize auth");
   }
   const onRedirectCallback = (appState?: AppState, user?: User) => {
+    navigate("/auth-callback");
     console.log("USER", user);
   };
   return (
