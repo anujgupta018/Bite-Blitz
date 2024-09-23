1723bdf7c81943abc0837b1d152e7e0e87529c76
Anuj <invincibleanuj1718@gmail.com>
2024-09-23 14:29:09 +0530

    Auth/Fixed bugs

--- Backend/src/middleware/auth.ts
+++ Backend/src/middleware/auth.ts
@@ -2,6 +2,6 @@ import { auth } from "express-oauth2-jwt-bearer";
 
 export const jwtCheck = auth({
   audience: process.env.AUTH0_AUDIENCE,
-  issuerBaseURL: process.env.AUTH0_ISUSER_BASE_URL,
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
   tokenSigningAlg: "RS256",
 });
