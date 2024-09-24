6bd714117ef3b51050dec8d6e18c53e522ad8dbb
Anuj <invincibleanuj1718@gmail.com>
2024-09-24 21:55:49 +0530

    Validation/Added

--- Backend/src/routes/myUserRoutes.ts
+++ Backend/src/routes/myUserRoutes.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import MyUserController from "../controllers/MyUserController";
 import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyUserRequest } from "../middleware/validation";
 
 const router = express.Router();
 
 // this path: /api/my/user
 router.post("/", jwtCheck, MyUserController.createCurrentUser);
-router.post("/", jwtCheck, jwtParse, MyUserController.updateCurrentUser);
+router.post(
+  "/",
+  jwtCheck,
+  jwtParse,
+  validateMyUserRequest,
+  MyUserController.updateCurrentUser
+);
 export default router;
